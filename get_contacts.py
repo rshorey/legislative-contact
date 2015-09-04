c574924a556d012167ea5385a0edefd883c6f053
Rachel Shorey <rshorey@gmail.com>
2015-09-04 19:27:14 -0400

    code to create a csv of state legislator contact info

---
+++ get_contacts.py
@@ -0,0 +1,44 @@
+import os
+import requests
+
+filename = 'contact.csv'
+outfile = open(filename, 'w')
+outfile.write('state,name,chamber,district,party,email,phone\n')
+outfile.close() #makes sure file is empty & has headers
+
+key='<your sunlight api key>'
+base_url = "http://openstates.org/api/v1/legislators/?apikey={key}&active=True"
+
+results = requests.get(base_url.format(key=key))
+results = results.json()
+
+
+with open(filename, 'a') as outfile:
+    for r in results:
+        name = r['full_name']
+        print(name)
+        try:
+            chamber = r['chamber']
+        except KeyError:
+            #this happens with weird peeps like tie-breaking lt govs.
+            chamber = ""
+        try:
+            district = r['district']
+        except KeyError:
+            district = ""
+        try:
+            party = r['party']
+        except KeyError:
+            party = ""
+        state = r['state']
+        email = ''
+        phone = ''
+        for o in r['offices']:
+            if 'email' in o and o['email']:
+                email = o['email']
+            if 'phone' in o and o['phone']:
+                phone = o['phone']
+        line = ','.join([state,name,chamber,district,party,email,phone])
+        outfile.write(line+'\n')
+
+
